f6572aab4b1d6359ac77edd649053125c41e7f11
Jean BRUTE-DE-REMUR <jean.brute-de-remur@igtp4-mac03.isim.intra>
2017-02-13 10:01:27 +0100

    Initial Commit

---
+++ ProjetiOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProjetiOS
+//
+//  Created by Jean BRUTE-DE-REMUR on 13/02/2017.
+//  Copyright © 2017 Jean BRUTE-DE-REMUR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
